ab356851458099272d653f19c7f6386565731449
Matheus Soares <matheusmatias2622@gmail.com>
2023-07-11 17:44:15 -0300

    feat: Adicionadas classes DTO

---
+++ src/main/java/br/com/banco/dto/TransferenciaDTO.java
@@ -0,0 +1,21 @@
+package br.com.banco.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransferenciaDTO {
+    private Long id;
+    private LocalDateTime dataTransferencia;
+    private BigDecimal valor;
+    private String tipo;
+    private String nomeOperadorTransacao;
+    private ContaDTO conta;
+}
+
