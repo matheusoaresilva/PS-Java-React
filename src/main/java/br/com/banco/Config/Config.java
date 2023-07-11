63207bd1ab72a5b6606337586953c59032c9e36d
Matheus Soares <matheusmatias2622@gmail.com>
2023-07-11 17:39:54 -0300

    feat: Adicionado modelmapper

--- src/main/java/br/com/banco/Config/Config.java
+++ src/main/java/br/com/banco/Config/Config.java
@@ -1,9 +1,15 @@
 package br.com.banco.Config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
 
 
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
